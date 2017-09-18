89d7718eb87475032e836ae8021c9057548f5869
Jochen-M <Jochen_M@163.com>
2017-09-18 13:43:47 +0800

    ,

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -1,8 +1,8 @@
-// export const SOCKET_HOST = 'http://localhost:3000';
-// export const SERVER_HOST = 'http://localhost:3000';
+export const SOCKET_HOST = 'http://localhost:3000';
+export const SERVER_HOST = 'http://localhost:3000';
 // export const SOCKET_HOST = 'http://192.168.31.189:3000';
 // export const SERVER_HOST = 'http://192.168.31.189:3000';
 // export const SOCKET_HOST = 'http://192.168.1.114:3000';
 // export const SERVER_HOST = 'http://192.168.1.114:3000';
-export const SOCKET_HOST = 'http://45.78.49.187:3001';
-export const SERVER_HOST = 'http://45.78.49.187:3001';
+// export const SOCKET_HOST = 'http://45.78.49.187:3001';
+// export const SERVER_HOST = 'http://45.78.49.187:3001';
